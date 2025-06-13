b4812536a836575c713e55ecc5c0557f6786a782
Aman Tripathi <123200498+aman73078@users.noreply.github.com>
2025-06-14 02:29:30 +0530

    layout created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { FeatureRoutes } from './features/features.route';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'dashboard',pathMatch:'full'},
+    {
+        path:'dashboard',
+        component: LayoutComponent,
+        children: FeatureRoutes
+    }
+];
