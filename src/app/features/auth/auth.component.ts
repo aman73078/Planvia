6238325dc35291c43de92a432cda0f778e2ca931
Aman Tripathi <123200498+aman73078@users.noreply.github.com>
2025-06-22 01:11:31 +0530

    updated

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,14 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-auth',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent {
-  
+  isLogin = true;
+  authData = { name: '', email: '', password: '', confirmPassword: '' };
 
-  
+  toggleMode() {
+    this.isLogin = !this.isLogin;
+    this.authData = { name: '', email: '', password: '', confirmPassword: '' };
+  }
+
+  submitForm() {
+    if (!this.isLogin && this.authData.password !== this.authData.confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+    alert(this.isLogin ? 'Logged in successfully!' : 'Account created successfully!');
+  }
 }
